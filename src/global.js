72dd4425018967a0fbb8c2ac99a587223981ad70
飞机上的长电视 <79362411+realtvop@users.noreply.github.com>
2025-01-09 16:00:38 +0800

    split msgHandler from global.js

--- src/global.js
+++ src/global.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue/dist/vue.esm-bundler";
 import * as VueRouter from "vue-router";
-import Notiflix from "notiflix";
+import { msgHandler } from "@utils/js/msgHandler";
 import eruda from "eruda";
 
 import packageConfig from "../package.json";
@@ -22,132 +22,9 @@ import "@utils/js/errHandler";
 if (import.meta.env.DEV) self.ptdb = ptdb;
 
 import { renderers } from "@utils/renderer";
-import { render } from "vue";
 
 var searchParams;
 
-const msgHandler = {
-  nodeText: document.getElementById("msg-out"),
-  lastMessage: "",
-  sendMessage(msg, type, system = true) {
-    if (type === "error") {
-      Notiflix.Notify.failure(msg, {
-        ID: "msgHandlerErr",
-        zindex: 114515,
-        cssAnimationStyle: "fade",
-        showOnlyTheLastOne: false,
-        opacity: "0.8",
-        borderRadius: "15px",
-      });
-    } else if (type === "success") {
-      Notiflix.Notify.success(msg, {
-        ID: "msgHandlerSuccess",
-        zindex: 114515,
-        cssAnimationStyle: "fade",
-        showOnlyTheLastOne: false,
-        opacity: "0.8",
-        borderRadius: "15px",
-      });
-    } else {
-      Notiflix.Notify.info(msg, {
-        ID: "msgHandlerInfo",
-        // showOnlyTheLastOne: true,
-        zindex: 114515,
-        cssAnimationStyle: "fade",
-        clickToClose: true,
-        showOnlyTheLastOne: false,
-        opacity: "0.8",
-        borderRadius: "15px",
-      });
-    }
-  },
-  confirm(msg, title, yes, no) {
-    if (!title) title = i18n.global.t("info.info");
-    if (!yes) yes = i18n.global.t("info.yes");
-    if (!no) no = i18n.global.t("info.no");
-    return new Promise((res) => {
-      Notiflix.Confirm.show(
-        title,
-        msg,
-        yes,
-        no,
-        () => {
-          res(true);
-        },
-        () => {
-          res(false);
-        },
-        {
-          zindex: 114515,
-          titleColor: "black",
-          okButtonBackground: "#90caf9",
-          messageMaxLength: 1000,
-          plainText: false,
-        }
-      );
-    });
-  },
-  info(msg, title, yes, type = "info") {
-    if (!title) title = i18n.global.t("info.info");
-    if (!yes) yes = i18n.global.t("info.ok");
-    return new Promise((res) => {
-      Notiflix.Report[type](
-        title,
-        msg,
-        yes,
-        () => {
-          res(true);
-        },
-        {
-          zindex: 114515,
-          titleColor: "black",
-          buttonBackground: "#90caf9",
-          svgSize: "30px",
-        }
-      );
-    });
-  },
-  success(msg, title, yes) {
-    if (!title) title = i18n.global.t("info.info");
-    if (!yes) yes = i18n.global.t("info.ok");
-    return this.info(msg, title, yes, "success");
-  },
-  failure(msg, title, yes) {
-    if (!title) title = i18n.global.t("info.failure");
-    if (!yes) yes = i18n.global.t("info.ok");
-    return this.info(msg, title, yes, "failure");
-  },
-  warning(msg, title, yes) {
-    if (!title) title = i18n.global.t("info.warning");
-    if (!yes) yes = i18n.global.t("info.ok");
-    return this.info(msg, title, yes, "warning");
-  },
-  prompt(msg, title, yes, no, placeholder = "") {
-    if (!title) title = i18n.global.t("info.info");
-    if (!yes) yes = i18n.global.t("info.confirm");
-    if (!no) no = i18n.global.t("info.cancel");
-    return new Promise(function (res) {
-      Notiflix.Confirm.prompt(
-        title,
-        msg,
-        placeholder,
-        yes,
-        no,
-        (clientAnswer) => {
-          res(clientAnswer);
-        },
-        (clientAnswer) => {
-          res(null);
-        },
-        {
-          zindex: 114515,
-          titleColor: "black",
-          okButtonBackground: "#90caf9",
-        }
-      );
-    });
-  },
-};
 ploading.init({
   whenShow: () => {
     ploading.tip = tipsHandler.getTip(ploading.msg);
