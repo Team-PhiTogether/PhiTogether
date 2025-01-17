14db272f653ebad3d9a99e8eb7bb4a1100d385a5
cgluWxh <wzb1244@qq.com>
2025-01-18 02:41:45 +0800

    Android-App-Fix

--- android/app/src/main/java/fun/phitogether/app/MainActivity.java
+++ android/app/src/main/java/fun/phitogether/app/MainActivity.java
@@ -1,5 +1,72 @@
 package fun.phitogether.app;
 
-import com.getcapacitor.BridgeActivity;
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
-public class MainActivity extends BridgeActivity {}
+import androidx.annotation.Nullable;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Logger;
+import com.getcapacitor.PluginLoadException;
+import com.getcapacitor.PluginManager;
+
+public class MainActivity extends BridgeActivity {
+
+    WebView webView = null;
+
+    private void makeItFullScreen() {
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            lp.layoutInDisplayCutoutMode = 3;
+        }
+        getWindow().setAttributes(lp);
+        WindowInsetsControllerCompat wic=new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
+        wic.hide(1|2|4);
+        wic.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        webView = getBridge().getWebView();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            webView.setForceDarkAllowed(false);
+        }
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.setVerticalScrollBarEnabled(false);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setSupportZoom(false);
+        webSettings.setBuiltInZoomControls(false);
+        webSettings.setDisplayZoomControls(false);
+
+
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        webView.evaluateJavascript("javascript:hook.pause();", null);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        makeItFullScreen();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        makeItFullScreen();
+    }
+}
