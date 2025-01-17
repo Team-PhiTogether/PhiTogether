82ae32b6559e97012fbd2fb5ae02f490f3c84427
飞机上的长电视 <79362411+realtvop@users.noreply.github.com>
2025-01-18 01:10:16 +0800

    [Capacitor] Add existing features & change icon for Android app

--- android/app/src/main/java/fun/phitogether/app/MainActivity.java
+++ android/app/src/main/java/fun/phitogether/app/MainActivity.java
@@ -2,4 +2,32 @@ package fun.phitogether.app;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
+public class MainActivity extends BridgeActivity {
+    private void makeItFullScreen() {
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            lp.layoutInDisplayCutoutMode = 3;
+        }
+        getWindow().setAttributes(lp);
+        WindowInsetsControllerCompat wic=new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
+        wic.hide(1|2|4);
+        wic.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+    }
+
+    @SuppressLint({"SetJavaScriptEnabled", "WrongConstant"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        makeItFullScreen();
+    }
+}
